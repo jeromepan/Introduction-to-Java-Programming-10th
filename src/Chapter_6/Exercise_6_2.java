57f66987c364f5fb69ad095b24102e9055dfd8e1
jerry <lmsj02@gmail.com>
2017-01-22 01:07:09 +0700

    Add files via upload

---
+++ src/Chapter_6/Exercise_6_2.java
@@ -0,0 +1,35 @@
+package Chapter_6;
+/*
+ * (Sum the digits in an integer) Write a method that computes the sum of the digits
+	in an integer. Use the following method header:
+	
+	public static int sumDigits(long n)
+	
+	For example, sumDigits(234) returns 9 (2 + 3 + 4). (Hint: Use the % operator to extract digits, and the / operator to remove the extracted digit. For instance,
+	to extract 4 from 234, use 234 % 10 (= 4). To remove 4 from 234, use 234 / 10
+	(= 23). Use a loop to repeatedly extract and remove the digit until all the digits
+	are extracted. Write a test program that prompts the user to enter an integer and
+	displays the sum of all its digits.
+ */
+
+import java.util.Scanner;
+
+public class Exercise_6_2 {
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Input an integer: ");
+		
+		long m = input.nextInt();
+		System.out.println(sumDigits(m));
+	}
+	
+	public static int sumDigits(long n){
+		int k = 0;
+		while(n!= 0){
+			k += n%10;		//if write to k = k + n%10; will trigger a problem.
+			n = n/10;
+		}
+		return k;
+	}
+	
+}
